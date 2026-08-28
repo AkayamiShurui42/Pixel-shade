0357ed92afc0819d73a6499b55a40f6896dd3d54
AkayamiShurui <61273606+AkayamiShurui42@users.noreply.github.com>
2026-08-28 08:44:00 -0400

    Add stock notification shade collapse support

--- app/src/main/java/com/crimson/pixelshade/PixelShadeAccessibilityService.kt
+++ app/src/main/java/com/crimson/pixelshade/PixelShadeAccessibilityService.kt
@@ -4,6 +4,23 @@ import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
 
 class PixelShadeAccessibilityService : AccessibilityService() {
+    companion object {
+        @Volatile private var instance: PixelShadeAccessibilityService? = null
+        fun requestCollapse() {
+            instance?.performGlobalAction(GLOBAL_ACTION_DISMISS_NOTIFICATION_SHADE)
+        }
+    }
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        instance = this
+    }
+
+    override fun onDestroy() {
+        if (instance === this) instance = null
+        super.onDestroy()
+    }
+
     override fun onAccessibilityEvent(event: AccessibilityEvent?) = Unit
     override fun onInterrupt() = Unit
 }
